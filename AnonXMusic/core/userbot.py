6f04382d40b1c9b8b3000650e943c7e2e9b74066
barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>
2024-02-05 00:48:40 +0700

    userbot.py
    Signed-off-by: barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -151,7 +151,7 @@ class Userbot(Client):
             self.five.name = self.five.me.mention
             self.five.username = self.five.me.username
             assistantids.append(self.five.id)
-            LOGGER(__name__).info(f"Assistant danfir Started as {self.five.name}")
+            LOGGER(__name__).info(f"Assistant 5 Started as {self.five.name}")
 
     async def stop(self):
         LOGGER(__name__).info(f"Stopping Assistants...")

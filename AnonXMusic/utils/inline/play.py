a97aaea007d09f225f91563a4ec5613927a75f33
barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>
2024-05-18 23:54:44 +0700

    play.py
    Signed-off-by: barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -31,23 +31,23 @@ def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    umm = math.floor(percentage)
-    if 0 < umm < 10:
-        bar = "¤————————"
-    elif 10 <= umm < 20:
-        bar = "—¤———————"
-    elif 20 <= umm < 30:
-        bar = "——¤——————"
-    elif 30 <= umm < 40:
-        bar = "———¤—————"
-    elif 40 <= umm < 50:
-        bar = "————¤————"
-    elif 50 <= umm < 60:
-        bar = "—————¤———"
-    elif 60 <= umm < 80:
-        bar = "——————¤——"
-    elif 80 <= umm < 95:
-        bar = "———————¤—"
+    great = math.floor(percentage)
+    if 0 < great < 10:
+        bar = "◉—————————"
+    elif 10 <= great < 20:
+        bar = "—◉————————"
+    elif 20 <= great < 30:
+        bar = "——◉———————"
+    elif 30 <= great < 40:
+        bar = "———◉——————"
+    elif 40 <= great < 50:
+        bar = "————◉—————"
+    elif 50 <= great < 60:
+        bar = "—————◉————"
+    elif 60 <= great < 80:
+        bar = "——————◉———"
+    elif 80 <= great < 95:
+        bar = "————————◉—"
     buttons = [
         [
             InlineKeyboardButton(
